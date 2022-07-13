7df46763322f063eb4724f760b8047884eccfd23
puqiong <pu11302817>
2022-07-13 17:43:50 +0800

    20220713 PQ 商户查询缓存 课程 并且完成作业 shop-type

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import static com.hmdp.utils.RedisConstants.SHOP_TYPE_LIST;
 
 /**
  * <p>
@@ -17,4 +28,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public List<ShopType> queryShopList() {
+        List<ShopType> list = new ArrayList<>();
+        // 先判断redis有没有该list
+        Set<String> range = stringRedisTemplate.opsForZSet().range(SHOP_TYPE_LIST, 0, -1);
+        if (!range.isEmpty()) {
+            for (String typeJson : range) {
+                list.add(JSONUtil.toBean(typeJson, ShopType.class, false));
+            }
+            return list;
+        }
+        // redis中没有 向数据库取
+        list = query().orderByAsc("sort").list();
+        // 向redis存
+        for (ShopType shopType : list) {
+            stringRedisTemplate.opsForZSet().add(SHOP_TYPE_LIST, JSONUtil.toJsonStr(shopType),shopType.getSort());
+        }
+
+        return list;
+    }
 }
