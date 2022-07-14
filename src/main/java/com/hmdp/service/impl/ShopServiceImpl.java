9dea476fc48fd4baeeb30eee9688c2dbbba27dfc
puqiong <pu11302817>
2022-07-14 17:25:41 +0800

    20220714 PQ 缓存穿透 之 返回空缓存处理办法

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -43,10 +42,16 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        if (shopJson != null) {
+            return Result.fail("店铺不存在");
+        }
         // 不存在 根据id查询数据库
         Shop shop = getById(id);
         // 不存在 返回错误
         if (shop == null) {
+            // 将空值写入 Redis 防止缓存穿透
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+
             return Result.fail("店铺不存在！");
         }
         // 存在 写入redis
